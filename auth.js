b4bfe68e161bcb75df5602fb50ec97d5b0e35c82
Sam Holmes <samholmes1337@gmail.com>
2016-12-29 15:28:37 +0000

    Tidy up formatting

--- auth.js
+++ auth.js
@@ -7,13 +7,11 @@ let path = require("path");
 
 let configPath = path.join(os.homedir(), "/.config/auth/tokens");
 
-// Check if the "tokens" file is present
 if (!fs.existsSync(configPath)) {
 	console.log(`Error: No token file found at ${configPath}`);
 	process.exit(1);
 }
 
-// Attempt to grab the tokens
 let tokens;
 
 try {
@@ -23,12 +21,10 @@ try {
 	process.exit(1);
 }
 
-// Get the arguments
 let args = process.argv.splice(2, process.argv.length);
 let argString = args.join(" ").toLowerCase();
 let found = false;
 
-// Called once found
 let success = output => {
 	found = true;
 	console.log(output);
@@ -40,27 +36,22 @@ let success = output => {
 	}
 }
 
-// Check them for names or alts
 tokens.forEach(token => {
 	let output = a.generateToken(token.secret);
 
-	// Check for the primary name
 	if (token.name.toLowerCase() === argString)
 		success(output);
 
-	// Check alternate names
 	token.alt.forEach(alt => {
 		if (argString === alt && !found)
 			success(output);
 	});
 });
 
-// Show error if short name is not found
 if (!found && args.length > 0) {
 	console.log(`Could not find a "${argString}" token`);
 }
 
-// If not shown, then show all
 if (args.length === 0) {
 
 	console.log(`Usage: auth <name|alt>
@@ -69,7 +60,7 @@ Generate your two factor authentication codes
 	`);
 
 	tokens.forEach(token => {
-		console.log("  " + token.name + " (" + token.alt.join(", ") + ")");
+		console.log(`  ${token.name} (${token.alt.join(", ")})`);
 	});
 
 	console.log();
